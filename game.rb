533642bbddd9bc49d1a3c464792a8e27872fcc17
caneroj1 <caneroj1@tcnj.edu>
2014-02-04 21:39:09 -0500

    Reorganized game.rb

--- game.rb
+++ game.rb
@@ -1,23 +1,33 @@
 require_relative 'gosu.rb'
 require_relative 'tumblr.rb'
 
-# ask for user input to get the blog name
-puts "Enter the name of the blog: "
-blogName = gets.chop
+class Game
 
-if blogName.empty? then 
-  puts "Error: Please enter a Tumblr blog name."
-else
-  ## create a new TumblrAPIObject so we can issue calls to the tumblr api for data
-  tumblr = TumblrAPIObject.new(blogName)
+  def initialize(w, h) # dimensions of 1250 x 900 are appropriate
+    @width = w
+    @height = h
+  end
+  
+  def start
+    # ask for user input to get the blog name
+    puts "Enter the name of the blog: "
+    blogName = gets.chop
 
-  ## query the API. if there were enough results then advance, else issue an error
-  if (tumblr.query == 5) then
-    tumblr.create_images                          # create and save the images for the game
+    if blogName.empty? then 
+      puts "Error: Please enter a Tumblr blog name."
+    else
+      ## create a new TumblrAPIObject so we can issue calls to the tumblr api for data
+      tumblr = TumblrAPIObject.new(blogName)
 
-    window = MyWindow.new(1250, 900)              # start the game
-    window.show
-  else
-    puts "Error downloading pictures from Tumblr. Please check your internet connection or the blog name you entered. Blogs need at least 5 pictures."
+      ## query the API. if there were enough results then advance, else issue an error
+      if (tumblr.query == 5) then
+        tumblr.create_images                                # create and save the images for the game
+
+        window = MyWindow.new(@width, @height)              # start the game
+        window.show
+      else
+        puts "Error downloading pictures from Tumblr. Please check your internet connection or the blog name you entered. Blogs need at least 5 pictures."
+      end
+    end
   end
 end
