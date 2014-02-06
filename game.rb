29e4cc6226d19c87825bb7fd4cb63159b207f0e2
caneroj1 <caneroj1@tcnj.edu>
2014-02-05 20:15:11 -0500

    Added cleanup functionality to the game. deletes pictures at the end.

--- game.rb
+++ game.rb
@@ -3,11 +3,15 @@ require_relative 'tumblr.rb'
 
 class Game
 
+  ## this function will accept the parameters that will decide the dimensions of the game window
   def initialize(w, h) # dimensions of 1250 x 900 are appropriate
     @width = w
     @height = h
   end
   
+  ## this function will initialize the gaming by asking the user which blogs they want to pull pictures from. there will be error checking in the
+  # tumblr.rb file. if there is an error the game will exit and display an error message. if everything succeeds the game will begin and go until the user wins,
+  # loses, or decides to exit
   def start
     # ask for user input to get the blog name
     puts "Enter the name of the blog: "
@@ -29,5 +33,20 @@ class Game
         puts "Error downloading pictures from Tumblr. Please check your internet connection or the blog name you entered. Blogs need at least 5 pictures."
       end
     end
+  end
+  
+  ## this function will delete all of the files that have been created to play the game.
+  def delete
+    File.delete("image1.png",
+                "image2.png",
+                "image3.png",
+                "image4.png",
+                "image5.png",
+                "image6.png",
+                "image7.png",
+                "image8.png",
+                "image9.png",
+                "image10.png",
+    )
   end
 end
